6a8c6838967f67d84c8ae4f332bb8d41790dc27d
nixberg <310825+nixberg@users.noreply.github.com>
2023-02-17 23:01:33 +0100

    Adopt std::simd

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,5 @@
+#![feature(portable_simd)]
+
 mod xoodoo;
 mod xoodyak;
 
--- src/xoodoo.rs
+++ src/xoodoo.rs
@@ -1,79 +1,102 @@
+use std::simd::{simd_swizzle, u32x4, u8x16};
+
 #[derive(Clone)]
 pub struct Xoodoo {
-    bytes: [u8; 48],
+    a: u32x4,
+    b: u32x4,
+    c: u32x4,
 }
 
 impl Xoodoo {
-    pub fn new() -> Xoodoo {
-        Xoodoo { bytes: [0; 48] }
+    pub fn new() -> Self {
+        Self {
+            a: u32x4::splat(0),
+            b: u32x4::splat(0),
+            c: u32x4::splat(0),
+        }
     }
 
     pub fn permute(&mut self) {
-        let mut words = [0u32; 12];
+        self.a.from_le();
+        self.b.from_le();
+        self.c.from_le();
 
-        self.unpack(&mut words);
-
-        let round_constants = &[
+        for round_constant in [
             0x058, 0x038, 0x3c0, 0x0d0, 0x120, 0x014, 0x060, 0x02c, 0x380, 0x0f0, 0x1a0, 0x012,
-        ];
+        ] {
+            let p = (self.a ^ self.b ^ self.c).rotate_lanes_right::<1>();
+            let e = p.rotate_left::<5>() ^ p.rotate_left::<14>();
+            self.a ^= e;
+            self.b ^= e;
+            self.c ^= e;
 
-        for &round_constant in round_constants {
-            let mut e = [0u32; 4];
+            self.b = self.b.rotate_lanes_right::<1>();
+            self.c = self.c.rotate_left::<11>();
 
-            for (i, e) in e.iter_mut().enumerate() {
-                let p = words[i] ^ words[i + 4] ^ words[i + 8];
-                *e = p.rotate_left(5) ^ p.rotate_left(14);
-            }
+            self.a ^= u32x4::from_array([round_constant, 0, 0, 0]);
 
-            for (i, word) in words.iter_mut().enumerate() {
-                *word ^= e[i.wrapping_sub(1) % 4];
-            }
+            self.a ^= !self.b & self.c;
+            self.b ^= !self.c & self.a;
+            self.c ^= !self.a & self.b;
 
-            words.swap(7, 4);
-            words.swap(7, 5);
-            words.swap(7, 6);
-
-            words[0] ^= round_constant;
-
-            for i in 0..4 {
-                let a = words[i + 0];
-                let b = words[i + 4];
-                let c = words[i + 8].rotate_left(11);
-
-                words[i + 8] = ((b & !a) ^ c).rotate_left(8);
-                words[i + 4] = ((a & !c) ^ b).rotate_left(1);
-                words[i] ^= c & !b;
-            }
-
-            words.swap(8, 10);
-            words.swap(9, 11);
+            self.b = self.b.rotate_left::<1>();
+            self.c = self.c.rho_east_part_two();
         }
 
-        self.pack(&words);
+        self.a.to_le();
+        self.b.to_le();
+        self.c.to_le();
     }
 
     #[inline(always)]
     pub fn bytes_view(&self) -> &[u8] {
-        &self.bytes
+        unsafe { core::slice::from_raw_parts(self as *const _ as *const u8, 48) }
     }
 
     #[inline(always)]
     pub fn bytes_view_mut(&mut self) -> &mut [u8] {
-        &mut self.bytes
+        unsafe { core::slice::from_raw_parts_mut(self as *mut _ as *mut u8, 48) }
+    }
+}
+
+trait XoodooInternal {
+    fn from_le(&mut self);
+
+    fn rotate_left<const OFFSET: u32>(&self) -> Self;
+
+    fn rho_east_part_two(&self) -> Self;
+
+    fn to_le(&mut self);
+}
+
+impl XoodooInternal for u32x4 {
+    #[inline(always)]
+    fn from_le(&mut self) {
+        self.as_mut_array()
+            .iter_mut()
+            .for_each(|lane| *lane = u32::from_le(*lane));
     }
 
-    #[inline]
-    fn unpack(&self, destination: &mut [u32; 12]) {
-        for (word, bytes) in destination.iter_mut().zip(self.bytes.chunks_exact(4)) {
-            *word = u32::from_le_bytes(bytes.try_into().unwrap());
-        }
+    #[inline(always)]
+    fn rotate_left<const OFFSET: u32>(&self) -> Self {
+        (self << Self::splat(OFFSET)) | (self >> Self::splat(32 - OFFSET))
     }
 
-    #[inline]
-    fn pack(&mut self, source: &[u32; 12]) {
-        for (bytes, word) in self.bytes.chunks_exact_mut(4).zip(source.iter()) {
-            bytes.copy_from_slice(&word.to_le_bytes());
-        }
+    #[inline(always)]
+    fn rho_east_part_two(&self) -> Self {
+        let mut bytes: u8x16 = unsafe { core::mem::transmute(*self) };
+        bytes = simd_swizzle!(
+            bytes,
+            [11, 8, 9, 10, 15, 12, 13, 14, 3, 0, 1, 2, 7, 4, 5, 6]
+        );
+        unsafe { core::mem::transmute(bytes) }
+    }
+
+    #[inline(always)]
+    fn to_le(&mut self) {
+        self.as_mut_array()
+            .iter_mut()
+            .for_each(|lane| *lane = lane.to_le());
     }
 }
 
@@ -90,7 +113,7 @@ mod tests {
         }
 
         assert_eq!(
-            xoodoo.bytes,
+            xoodoo.bytes_view(),
             [
                 0xb0, 0xfa, 0x04, 0xfe, 0xce, 0xd8, 0xd5, 0x42, 0xe7, 0x2e, 0xc6, 0x29, 0xcf, 0xe5,
                 0x7a, 0x2a, 0xa3, 0xeb, 0x36, 0xea, 0x0a, 0x9e, 0x64, 0x14, 0x1b, 0x52, 0x12, 0xfe,
